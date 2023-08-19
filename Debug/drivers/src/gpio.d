a3e71a597173cb87e3be1c5a8daec7c7eaa427b9
mohammed-444 <mohamedalaa20050@gmail.com>
2023-08-19 13:34:54 +0300

    added debug

---
+++ Debug/drivers/src/gpio.d
@@ -0,0 +1,22 @@
+drivers/src/gpio.o: ../drivers/src/gpio.c \
+ E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/device/stm32f4xx.h \
+ E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/core/core_cm4.h \
+ E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/core/core_cmInstr.h \
+ E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/core/core_cmFunc.h \
+ E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/core/core_cmSimd.h \
+ E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/device/system_stm32f4xx.h \
+ E:/SBME3/second\ term/embedded/workspace/lab4/drivers/inc/gpio.h
+
+E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/device/stm32f4xx.h:
+
+E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/core/core_cm4.h:
+
+E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/core/core_cmInstr.h:
+
+E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/core/core_cmFunc.h:
+
+E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/core/core_cmSimd.h:
+
+E:/SBME3/second\ term/embedded/workspace/lab4/CMSIS/device/system_stm32f4xx.h:
+
+E:/SBME3/second\ term/embedded/workspace/lab4/drivers/inc/gpio.h:
